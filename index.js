e9ef1eb2f3c808a63bcda8c11a1b85875d10d125
joamag <joamag@hive.pt>
2020-06-05 11:14:49 +0100

    New verbosity

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const util = require("util");
+const process = require("process");
 const core = require("@actions/core");
 
 const exec = util.promisify(require("child_process").exec);
@@ -7,12 +8,17 @@ async function removeTemp(tempPath = "/tmp") {
     const { stdout, stderr } = await exec(`rm -rf ${tempPath}`);
     if (stdout) console.log(stdout);
     if (stderr) console.error(stderr);
+    console.warn(`Removed ${tempPath} directory`);
 }
 
 async function removeTools(toolsEnv = "AGENT_TOOLSDIRECTORY") {
+    if (process.env[toolsEnv] === undefined) {
+        console.warn(`Skipping tools removal, ${toolsEnv} env does not exist`);
+    }
     const { stdout, stderr } = await exec(`rm -rf $${toolsEnv}`);
     if (stdout) console.log(stdout);
     if (stderr) console.error(stderr);
+    console.warn(`Removed directory from ${toolsEnv} env`);
 }
 
 async function removeAll() {
